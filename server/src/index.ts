d9d6d16bba29d6f408fb2f449f8133719ee3f355
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-04-25 17:50:50 -0400

    Update : table names for index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,20 @@
 import { app as server } from './server';
-import { db } from './db';
+import { db, config } from './db';
 
 const main = async () => {
-	const PORT = process.env.PORT || 5000;
-	/* move to script to execute
-	await db.query('DROP TABLE IF EXISTS google_auth.users;');
-	await db.query(`CREATE TABLE google_auth.users (
+  const PORT = process.env.PORT || 5000;
+  /* move to script to execute
+  await db.query(`DROP TABLE IF EXISTS ${config.TABLE_NAMES.users};`);
+  await db.query(`CREATE TABLE ${config.TABLE_NAMES.users}(
 			id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 			first_name VARCHAR(50),
 			last_name VARCHAR(50),
 			email VARCHAR(50) NOT NULL
 	);`);
 	*/
-	server.listen(PORT, () => {
-		console.log(`Listening on port ${PORT}`);
-	});
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
 };
 
 main();
